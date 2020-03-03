135dc367f569e8767958173b3122952d1490c565
mori yuta <59682979+uta-mori@users.noreply.github.com>
2020-03-03 11:22:40 +0900

    spek対応？

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,10 +11,17 @@ repositories {
 dependencies {
 	implementation(kotlin("stdlib-jdk8"))
 	testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
+
+    	testImplementation("org.spekframework.spek2:spek-dsl-jvm:$spek_version")
+    	testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:$spek_version")
+    	// spek requires kotlin-reflect, can be omitted if already in the classpath
+    	testRuntimeOnly("org.jetbrains.kotlin:kotlin-reflect:$kotlin_version")
 }
 
 tasks.test {
-	useJUnitPlatform()
+	useJUnitPlatform {
+		includeEngines 'spek2'
+	}
 	testLogging {
 		events("passed", "skipped", "failed")
 	}
